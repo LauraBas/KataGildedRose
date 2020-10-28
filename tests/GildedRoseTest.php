6bc931c359b03ceb0dc6941011075908567e5956
Laura Bassani <bassanilaurarita@gmail.com>
2020-10-28 10:19:19 +0100

    Add test if sellin pass quality reduces twice, quality is always positive and less than 50, brie inccrease quality if sellin pass

--- tests/GildedRoseTest.php
+++ tests/GildedRoseTest.php
@@ -17,4 +17,37 @@ class GildedRoseTest extends TestCase
 
 		$this->assertEquals(2, $someItem->quality);
 	}
+
+	public function test_when_sellin__pass_quality_decrease_twice()
+	{
+		$someItem = new Item("Some Item", 0, 3);
+
+		GildedRose::updateQuality([$someItem]);
+
+		$this->assertEquals(1, $someItem->quality);
+	}
+	public function test_quality_is_always_positive()
+	{
+		$someItem = new Item("Some Item", 0, 1);
+
+		GildedRose::updateQuality([$someItem]);
+
+		$this->assertEquals(0, $someItem->quality);
+	}
+	public function test_brie_increase_quality()
+	{
+		$someItem = new Item("Aged Brie", 1, 1);
+
+		GildedRose::updateQuality([$someItem]);
+
+		$this->assertEquals(2, $someItem->quality);
+	}
+	public function test_quality_is_always_less_50()
+	{
+		$someItem = new Item("Aged Brie", 1, 50);
+
+		GildedRose::updateQuality([$someItem]);
+
+		$this->assertEquals(50, $someItem->quality);
+	}
 }
